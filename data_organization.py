db11fb7093dd5d769d3edb028e870cea8d83d4e1
Ananya Purwar <apurwar@teapot.cs.hmc.edu>
2025-07-31 06:13:11 -0700

    general model working

--- data_organization.py
+++ data_organization.py
@@ -18,8 +18,6 @@ out_labels.mkdir(parents=True, exist_ok=True)
 for i, folder in enumerate(sorted(src_dir.glob("BraTS-MET-*"))):
     if not folder.is_dir():
         continue
-    if i >= 1:  # Limit to first 20 cases
-        break
     case_id = folder.name
     t1n = folder / f"{case_id}-t1n.nii.gz"
     t1c = folder / f"{case_id}-t1c.nii.gz"
@@ -29,13 +27,13 @@ for i, folder in enumerate(sorted(src_dir.glob("BraTS-MET-*"))):
     default_seg   = folder / f"{case_id}-seg.nii.gz"
     seg = corrected_seg if corrected_seg.exists() else default_seg
     
-    print(f"Checking {case_id}")
-    print("  Expected files:")
-    print(f"    {t1n.exists()=}, {t1n}")
-    print(f"    {t1c.exists()=}, {t1c}")
-    print(f"    {t2w.exists()=}, {t2w}")
-    print(f"    {t2f.exists()=}, {t2f}")
-    print(f"    {seg.exists()=}, {seg}")
+    # print(f"Checking {case_id}")
+    # print("  Expected files:")
+    # print(f"    {t1n.exists()=}, {t1n}")
+    # print(f"    {t1c.exists()=}, {t1c}")
+    # print(f"    {t2w.exists()=}, {t2w}")
+    # print(f"    {t2f.exists()=}, {t2f}")
+    # print(f"    {seg.exists()=}, {seg}")
 
     if not all(f.exists() for f in [t1n, t1c, t2w, t2f, seg]):
         print(f"Missing file in {case_id}, skipping.")
--- model.py
+++ model.py
@@ -1,24 +1,34 @@
-import json
-from pathlib import Path
 from monai.apps.auto3dseg import AutoRunner
+from pathlib import Path
+import json
 
-# Load existing dataset.json
-with open("/mnt/cs/cs153/data/brats_data_purwar/BraTS-MET2025/dataset.json") as f:
-    config = json.load(f)
+def main():
+    data_dir = Path("/mnt/cs/cs153/data/brats_data_purwar/BraTS-MET2025")
+    output_dir = Path("./autoseg_output")
 
-# Extend config with required fields
-config["dataroot"] = "/mnt/cs/cs153/data/brats_data_purwar/BraTS-MET2025"
-config["datalist"] = "/mnt/cs/cs153/data/brats_data_purwar/BraTS-MET2025/dataset.json"
-config["modality"] = {str(i): m for i, m in enumerate(["t1n","t1c","t2w","t2f"])}
+    with open(data_dir / "dataset.json") as f:
+        config = json.load(f)
 
-# Initialize runner
-runner = AutoRunner(
-    input=config,
-    work_dir="autoseg_output",
-    train=True,
-    analyze=True,
-    predict=False,
-    infer=False
-)
+    config["dataroot"] = str(data_dir)
+    config["datalist"] = str(data_dir / "dataset.json")
+    config["modality"] = "MRI"
+    config["channel_definitions"] = {
+    "t1n": "T1-weighted non-contrast",
+    "t1c": "T1-weighted contrast-enhanced",
+    "t2w": "T2-weighted",
+    "t2f": "FLAIR"
+    }
 
-runner.run()
+    runner = AutoRunner(
+        input=config,
+        work_dir=output_dir,
+        train=True,
+        analyze=True,
+        predict=False,
+        infer=False
+    )
+
+    runner.run()
+
+if __name__ == "__main__":
+    main()
